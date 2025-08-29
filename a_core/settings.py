31f39eb002f55390663920f032f76c90f8296331
Bishal Rokaha <rokahabishal173@gmail.com>
2025-08-29 19:08:10 +0530

    let's go

--- a_core/settings.py
+++ a_core/settings.py
@@ -38,14 +38,14 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG=False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'real-time-chat-pdj8.onrender.com']
 
 INTERNAL_IPS = (
     '127.0.0.1',
     'localhost:8000'
 )
 
-CSRF_TRUSTED_ORIGINS = [ 'https://*' ]
+CSRF_TRUSTED_ORIGINS = [ 'https://real-time-chat-pdj8.onrender.com' ]
 
 
 # Application definition
